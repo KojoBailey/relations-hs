af9f94e5c6d056a57976d6c8c0710fae5547ce62
Kojo Bailey <kojomolojo@gmail.com>
2025-12-30 11:18:25 +0800

    Add checks for reflexive, symmetric, anti-symmetric, and transitive

--- Main.hs
+++ Main.hs
@@ -14,3 +14,19 @@ relationOnPairs relation pairs = [x | x <- pairs, relation x]
 
 relationOnSet :: Relation a a -> Set a -> Set (a, a)
 relationOnSet relation set = relationOnPairs relation (cartesianProduct set set)
+
+extrapolateSetFromPairs :: Eq a => Set (a, a) -> Set a
+extrapolateSetFromPairs pairs = toSet $ foldr (\(x, y) acc -> x : y : acc) [] pairs
+
+isReflexive :: Eq a => Set (a, a) -> Bool
+isReflexive pairs = all (\x -> (x, x) `elem` pairs) set
+  where set = extrapolateSetFromPairs pairs
+
+isSymmetric :: Eq a => Set (a, a) -> Bool
+isSymmetric pairs = all (\(x, y) -> (y, x) `elem` pairs) pairs
+
+isAntiSymmetric :: Eq a => Set (a, a) -> Bool
+isAntiSymmetric pairs = all (\(x, y) -> x == y || (y, x) `notElem` pairs) pairs
+
+isTransitive :: Eq a => Set (a, a) -> Bool
+isTransitive pairs = all (\(x1, y1) -> all (\(x2, y2) -> y1 /= x2 || (x1, y2) `elem` pairs) pairs) pairs
